418a0b10218b80b7e53fc71c0e3cd77579c8f8ef
tiagodalmeida <dalmeidatiago@gmail.com>
2024-08-07 14:25:44 -0300

    First Steps In Java With Spring Boot 4

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dalmeida/tiago/Startup.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dalmeida/tiago/Startup.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.dalmeida.tiago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/dalmeida/tiago/StartupTests.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/dalmeida/tiago/StartupTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.dalmeida.tiago;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
