82be8baceffa76ba969501a51430e84d20654f97
tiagodalmeida <dalmeidatiago@gmail.com>
2024-08-12 19:05:13 -0300

    CRUD de person

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dalmeida/tiago/controller/PersonController.java
@@ -0,0 +1,45 @@
+package br.com.dalmeida.tiago.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import br.com.dalmeida.tiago.model.Person;
+import br.com.dalmeida.tiago.service.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	private final PersonService personService;
+
+    PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+    
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return personService.findById(id);
+    }
+    
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findByAll() {
+        return personService.findByAll();
+    }
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+    
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person updatePerson(@RequestBody Person person) {
+        return personService.updatePerson(person);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public void deletePerson(@PathVariable(value = "id") String id) {
+        personService.deletePerson(id);
+    }
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/dalmeida/tiago/service/PersonService.java
@@ -0,0 +1,69 @@
+package br.com.dalmeida.tiago.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.dalmeida.tiago.model.Person;
+
+@Service
+public class PersonService {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	private Logger logger = Logger.getLogger(PersonService.class.getName());
+	
+	public List<Person> findByAll() {
+		List<Person> persons = new ArrayList<>();
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+
+	public Person findById(String id) {
+		
+		logger.info("Finding a person!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Tiago");
+		person.setLastName("Goncalves");
+		person.setAddress("Rio de Janeiro, Rio de Janeiro - Brazil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public Person createPerson(Person person) {
+		logger.info("Creating a person!");
+		return person;
+	}
+	
+	public Person updatePerson(Person person) {
+		logger.info("Updating a person!");
+		return person;
+	}
+	
+	public void deletePerson(String id) {
+		logger.info("Deleting a person!");
+	}
+	
+	private Person mockPerson(int i) {
+
+		logger.info("Finding all persons!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person firstName " + i);
+		person.setLastName("Person lastName " + i);
+		person.setAddress("Some address in Brazil " + i);
+		person.setGender("Male");
+		
+		return person;
+	}
+}
